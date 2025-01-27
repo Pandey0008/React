acd8b0ccac8381d87326aa9f51d24938f6313c36
Your Name <your-email@example.com>
2025-01-27 17:27:07 +0530

    done

---
+++ 14blogapp/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <img  className="w-7 mx-2" src="https://w7.pngwing.com/pngs/748/81/png-transparent-computer-icons-blogger-logo-blogger-logo-miscellaneous-rectangle-wood-thumbnail.png" />
+  )
+}
+
+export default Logo
